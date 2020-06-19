1cc2ff4cf58812e0c3aa13446db72103a0d67960
Jasjot Singh <jasjot784@gmail.com>
2020-06-19 16:30:27 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -10,9 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast toast = Toast.makeText(MainActivity.this, "toast" , Toast.LENGTH_LONG);//first parameter is context and third is duration also can be written as a number which is in milliseconds
+        Toast toast = Toast.makeText(MainActivity.this, "toast" , Toast.LENGTH_LONG);//first parameter is context and third is duration 
         toast.show();//or .show without making object
         //we can specify anything in custom toast
     }
 }
-// toST IS SHORT POPUP messsage
+// toaST IS SHORT POPUP messsage
